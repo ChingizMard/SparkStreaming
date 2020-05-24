cd2f8ff0b7b77cc77cfb92db19c2d8353fc5ee45
Chingiz <chingiz@li.ru>
2020-05-23 20:42:50 -0400

    Twitter hashtag streaming example.

--- build.sbt
+++ build.sbt
@@ -8,5 +8,6 @@ scalaVersion := "2.11.2"
 libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-core_2.11" % "2.2.0",
   "org.apache.spark" % "spark-sql_2.11" % "2.2.0",
-  "org.apache.spark" % "spark-streaming_2.11" % "2.2.0"
+  "org.apache.spark" % "spark-streaming_2.11" % "2.2.0",
+  "org.apache.bahir" %% "spark-streaming-twitter" % "2.2.0"
 )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.{SparkConf, SparkContext, streaming}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.twitter._
 
 /*
 This is the netcat to start the socket:
@@ -8,20 +9,53 @@ nc -l 127.0.0.1 2222
  */
 
 
-object HelloWorld {
+object TwitterHashtags {
+
+  val conf = new SparkConf().setMaster("local[4]").setAppName("Spark Streaming - PopularHashTags")
+  val sc = new SparkContext(conf)
+  Logger.getLogger("org").setLevel(Level.OFF)
 
   def main(args: Array[String]): Unit = {
-    //Disable All INFO Statements
-    Logger.getLogger("org").setLevel(Level.OFF)
+    val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
+    val filters = args.takeRight(args.length - 4)
 
-    val conf = new SparkConf().setAppName("Basic Stream NetCat").setMaster("local[2]")
-    val sc = new SparkContext(conf)
-    val ssc = new StreamingContext(sc, Seconds(10))
-    val streamRDD = ssc.socketTextStream("127.0.0.1", 2222)
-    val wc = streamRDD.flatMap(line => line.split(" "))
-                      .map(word => (word, 1))
-                      .reduceByKey(_+_)
-    wc.print()
+    System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+    System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+    System.setProperty("twitter4j.oauth.accessToken", accessToken)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+
+    // Set the Spark StreamingContext to create a DStream for every 5 seconds
+    val ssc = new StreamingContext(sc, Seconds(5))
+    // Pass the filter keywords as arguements
+
+    //  val stream = FlumeUtils.createStream(ssc, args(0), args(1).toInt)
+    val stream = TwitterUtils.createStream(ssc, None, filters)
+    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+    // Get the top hashtags over the previous 60 sec window
+    val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+      .map { case (topic, count) => (count, topic) }
+      .transform(_.sortByKey(false))
+
+    // Get the top hashtags over the previous 10 sec window
+    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+      .map { case (topic, count) => (count, topic) }
+      .transform(_.sortByKey(false))
+
+    // print tweets in the currect DStream
+    stream.print()
+
+    // Print popular hashtags
+    topCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+    topCounts10.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
 
     ssc.start()
     ssc.awaitTermination()
